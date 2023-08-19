d10c35e9d0bb82be8efaa56899fa6bbcf30262f5
Omnia <100493759+Omnia-Bashandy@users.noreply.github.com>
2023-08-19 18:53:22 +0300

    Add files via upload

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
-#inclde "main.h"
+#include "main.h"
 
 /**
   * main - Test function for positive or negative
   * Return: 0
   */
+
 int main(void)
 {
-	int i;
+int i;
 
-	i = 98 % 2;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
+return (0);
 }

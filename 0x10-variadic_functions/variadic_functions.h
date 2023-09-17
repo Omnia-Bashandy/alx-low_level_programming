cfd2515cbe6095386b60315242b2a784e4fcdc54
Omnia <100493759+Omnia-Bashandy@users.noreply.github.com>
2023-09-17 16:07:33 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,11 +4,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-typedef struct printer{
-char *symbol;
-void (*print)(va_list arg);
+typedef struct printer {
+    char *symbol;
+    void (*print)(va_list arg);
 } printer_t;
-
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -19,4 +18,4 @@ void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
-#endif
+#endif /* VARIADIC_FUNCTIONS_H */
